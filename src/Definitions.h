591751b72c9fe15da04c4166bdd5d59cf8e0a856
Nicolas Blanchard <nblanchardaz@gmail.com>
2024-09-01 19:13:30 -0700

    Updated UUIDs.

--- src/Definitions.h
+++ src/Definitions.h
@@ -4,12 +4,12 @@
 #define strobePinDef D2
 #define resetPinDef D3
 #define dcOutPinDef D1
-#define primaryStartingUUID uint16_t(11)        // MUST BE 16, 32, or 128 BITS
-#define primaryEndingUUID uint16_t(12)          // MUST BE 16, 32, or 128 BITS
-#define primarySpeedUUID uint16_t(13)           // MUST BE 16, 32, or 128 BITS
-#define secondaryStartingUUID uint16_t(14)      // MUST BE 16, 32, or 128 BITS
-#define secondaryEndingUUID uint16_t(15)        // MUST BE 16, 32, or 128 BITS
-#define secondarySpeedUUID uint16_t(16)         // MUST BE 16, 32, or 128 BITS
-#define protocolUUID uint16_t(17)               // MUST BE 16, 32, OR 128 BITS
+#define primaryStartingUUID uint16_t(1)        // MUST BE 16, 32, or 128 BITS
+#define primaryEndingUUID uint16_t(2)          // MUST BE 16, 32, or 128 BITS
+#define primarySpeedUUID uint16_t(3)           // MUST BE 16, 32, or 128 BITS
+#define secondaryStartingUUID uint16_t(4)      // MUST BE 16, 32, or 128 BITS
+#define secondaryEndingUUID uint16_t(5)        // MUST BE 16, 32, or 128 BITS
+#define secondarySpeedUUID uint16_t(6)         // MUST BE 16, 32, or 128 BITS
+#define protocolUUID uint16_t(7)               // MUST BE 16, 32, OR 128 BITS
 #define SERVICE_UUID uint16_t(1234)             // MUST BE 16, 32, or 128 BITS
 #define DEVICE_NAME "LED CONTROLLER"
